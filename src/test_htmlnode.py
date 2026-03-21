3fa23122cff4d8aea82267d04a8e5f5f4e451f21
Cmonster820 <Cmonster820@aol.com>
2026-03-21 18:37:48 -0400

    l4

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -3,7 +3,7 @@ import unittest
 from htmlnode import *
 
 
-class TestTextNode(unittest.TestCase):
+class TestHTMLNode(unittest.TestCase):
     def test_constructor(self):
         node = HTMLNode("a", "meme", [], {})
         self.assertEqual(node.__repr__(), "HTMLNode(a,meme,[],{})")
@@ -15,6 +15,19 @@ class TestTextNode(unittest.TestCase):
     def test_children(self):
         node = HTMLNode("a", "meme", [HTMLNode()], {})
         self.assertEqual(node.__repr__(), "HTMLNode(a,meme,[HTMLNode(None,None,None,None)],{})")
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_repr(self):
+        node = LeafNode(None, "meme",{})
+        self.assertEqual(node.__repr__(),"LeafNode(None,meme,{})")
+
+    def test_leaf_a_to_html(self):
+        node = LeafNode("a","meme",{"href": "https://www.google.com","target": "_blank",})
+        self.assertEqual(node.to_html(), "<a href=\"https://www.google.com\" target=\"_blank\">meme</a>")
 
 if __name__ == "__main__":
     unittest.main()
