f9ee998a2911995637730a7ec219a01c64f524fa
Cmonster820 <Cmonster820@aol.com>
2026-03-21 21:21:25 -0400

    add support for parent nodes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -33,3 +33,18 @@ class LeafNode(HTMLNode):
     
     def __repr__(self):
         return f"LeafNode({self.tag},{self.value},{self.props})"
+    
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("no tag")
+        if self.children is None:
+            raise ValueError("no children")
+        strout = f"<{self.tag}{self.props_to_html()}>"
+        for child in self.children:
+            strout+=child.to_html()
+        return strout+"</"+self.tag+">"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -29,5 +29,25 @@ class TestLeafNode(unittest.TestCase):
         node = LeafNode("a","meme",{"href": "https://www.google.com","target": "_blank",})
         self.assertEqual(node.to_html(), "<a href=\"https://www.google.com\" target=\"_blank\">meme</a>")
 
+class TestParentNode(unittest.TestCase):
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+    def test_to_html_with_no_children(self):
+        node = ParentNode("p", [])
+        self.assertEqual(node.to_html(), "<p></p>")
+    
+
 if __name__ == "__main__":
     unittest.main()
